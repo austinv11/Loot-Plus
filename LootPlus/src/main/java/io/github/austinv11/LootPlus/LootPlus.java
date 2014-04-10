8c12a443be0e3121d8d1ac4dfeb6ef3b49154ab6
austinv11 <austinv11@gmail.com>
2014-04-10 19:09:58 -0400

    Implemented the Options.setToDefault config option

--- LootPlus/src/main/java/io/github/austinv11/LootPlus/LootPlus.java
+++ LootPlus/src/main/java/io/github/austinv11/LootPlus/LootPlus.java
@@ -1,38 +1,67 @@
 package io.github.austinv11.LootPlus;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class LootPlus extends JavaPlugin{
+public class LootPlus extends JavaPlugin implements Listener{
 	public String CURRENT_VERSION = "1.0.0"; //TODO remember to update
+	FileConfiguration config = getConfig();
+	PluginManager pm = Bukkit.getServer().getPluginManager();
 	@Override
 	public void onEnable(){
-		configInit();
+		configInit(false);
+		if (config.getBoolean("Options.setToDefault") == true){
+			configInit(true);
+		}
+		pm.registerEvents(this, this);
 		getLogger().info("Loot on this server is now enhanced by LootPlus V"+CURRENT_VERSION+"!");
 	}
-	public void configInit(){
-		getLogger().info("Initiating config...");
-		FileConfiguration config = getConfig();
-		config.addDefault("Options.onlyCustomDrops", "TODO");//FIXME
-		config.addDefault("Options.allowCustomDrops", "TODO");//FIXME
-		config.addDefault("Options.allowCustomXP", "TODO");//FIXME
-		config.addDefault("Options.allowCustomDungeonLoot", "TODO");//FIXME
-		config.addDefault("Options.allowCustomSpawnRate", "TODO");//FIXME
-		config.addDefault("Options.updateNotifications", "TODO");//FIXME
-		config.addDefault("Options.setToDefault", "TODO");//FIXME
-		config.addDefault("Features.headDrops", "TODO");//FIXME
-		config.addDefault("Features.playerHeadDrops", "TODO");//FIXME
-		config.addDefault("Features.extraHeadDrops", "TODO");//FIXME
-		config.addDefault("Features.extraDungeonLoot", "TODO");//FIXME
-		config.addDefault("Features.extraMobDrops", "TODO");//FIXME
-		config.addDefault("Features.extraEnchantments", "TODO");//FIXME
-		config.addDefault("Features.extraDungeons", "TODO");//FIXME
-		config.addDefault("Features.bossMobs", "TODO");//FIXME
-		config.options().copyDefaults(true);
-		saveConfig();
-		getLogger().info("Initiated config!");
+	public void configInit(boolean override){
+		if (override == false){
+			getLogger().info("Initiating config...");
+			config.addDefault("Options.onlyCustomDrops", "TODO");//FIXME
+			config.addDefault("Options.allowCustomDrops", "TODO");//FIXME
+			config.addDefault("Options.allowCustomXP", "TODO");//FIXME
+			config.addDefault("Options.allowCustomDungeonLoot", "TODO");//FIXME
+			config.addDefault("Options.allowCustomSpawnRate", "TODO");//FIXME
+			config.addDefault("Options.updateNotifications", "TODO");//FIXME
+			config.addDefault("Options.setToDefault", false);
+			config.addDefault("Features.headDrops", "TODO");//FIXME
+			config.addDefault("Features.playerHeadDrops", "TODO");//FIXME
+			config.addDefault("Features.extraHeadDrops", "TODO");//FIXME
+			config.addDefault("Features.extraDungeonLoot", "TODO");//FIXME
+			config.addDefault("Features.extraMobDrops", "TODO");//FIXME
+			config.addDefault("Features.extraEnchantments", "TODO");//FIXME
+			config.addDefault("Features.extraDungeons", "TODO");//FIXME
+			config.addDefault("Features.bossMobs", "TODO");//FIXME
+			config.options().copyDefaults(true);
+			saveConfig();
+			getLogger().info("Initiated config!");
+		} else if (override == true){
+			getLogger().info("Reverting config to defaults...");
+			config.set("Options.onlyCustomDrops", "TODO");//FIXME
+			config.set("Options.allowCustomDrops", "TODO");//FIXME
+			config.set("Options.allowCustomXP", "TODO");//FIXME
+			config.set("Options.allowCustomDungeonLoot", "TODO");//FIXME
+			config.set("Options.allowCustomSpawnRate", "TODO");//FIXME
+			config.set("Options.updateNotifications", "TODO");//FIXME
+			config.set("Options.setToDefault", false);
+			config.set("Features.headDrops", "TODO");//FIXME
+			config.set("Features.playerHeadDrops", "TODO");//FIXME
+			config.set("Features.extraHeadDrops", "TODO");//FIXME
+			config.set("Features.extraDungeonLoot", "TODO");//FIXME
+			config.set("Features.extraMobDrops", "TODO");//FIXME
+			config.set("Features.extraEnchantments", "TODO");//FIXME
+			config.set("Features.extraDungeons", "TODO");//FIXME
+			config.set("Features.bossMobs", "TODO");//FIXME
+			saveConfig();
+			getLogger().info("Reverted config!");
+		}
 	}
 	@Override
 	public void onDisable(){
