32b4dbc84359dd5d5930e938e86a7ddd6531ddf6
austinv11 <austinv11@gmail.com>
2014-04-10 19:22:56 -0400

    removed placeholders
    and sorta implemented the /tweaks plugins

--- LootPlus/src/main/java/io/github/austinv11/LootPlus/LootPlus.java
+++ LootPlus/src/main/java/io/github/austinv11/LootPlus/LootPlus.java
@@ -1,6 +1,7 @@
 package io.github.austinv11.LootPlus;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -70,6 +71,10 @@ public class LootPlus extends JavaPlugin implements Listener{
 	}
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		if (cmd.getName().equalsIgnoreCase("tweaks")) {
+			sender.sendMessage(ChatColor.YELLOW+"Current tweaks implemented by LootPlus:");
+			//TODO
+		}
 		return false;
 	}
 }
