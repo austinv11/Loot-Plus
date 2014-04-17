890123c8e80f427982186a5cebb0663082493291
austinv11 <austinv11@gmail.com>
2014-04-17 11:09:58 -0400

    Had to remove the biome option

--- LootPlus/src/main/java/io/github/austinv11/LootPlus/CustomDungeonLootInterface.java
+++ LootPlus/src/main/java/io/github/austinv11/LootPlus/CustomDungeonLootInterface.java
@@ -3,7 +3,6 @@ package io.github.austinv11.LootPlus;
 import java.io.File;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
@@ -25,8 +24,7 @@ public class CustomDungeonLootInterface implements Listener{
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 		dungeonInit();
 	}
-	public void dungeonInit(){//biomes must be Biomes ('all' to be ignored) (defined here: http://jd.bukkit.org/beta/doxygen/dc/d5c/enumorg_1_1bukkit_1_1block_1_1Biome.html)
-		dungeonDatas.addDefault("biome", "[all]"); //all means the biome doesn't matter
+	public void dungeonInit(){
 		dungeonDatas.addDefault("probability", "[0]");
 		dungeonDatas.addDefault("loot", "[none]");
 		dungeonDatas.options().copyDefaults(true);
@@ -55,22 +53,16 @@ public class CustomDungeonLootInterface implements Listener{
 				}else{
 					Chest chest = (Chest) state;
 					Inventory cInv = chest.getInventory();
-					String[] biome = readTable(dungeonDatas.getString("biome"));
 					String[] probability = readTable(dungeonDatas.getString("probability"));
 					String[] loot = readTable(dungeonDatas.getString("loot"));
 					if (probability[0] != "0" && loot[0] != "none"){
-						if (probability.length == loot.length && biome.length == loot.length){
+						if (probability.length == loot.length){
 							for (int i = 0; i < loot.length; i++){
 								double lootChance = Math.random();
 								double chance = (Double.parseDouble(probability[i].replace("‘", "").replace("’", "")) / 100);
 								if (lootChance <= chance){
-									Location loc = chest.getLocation().clone();
 									Material lookup = Material.getMaterial(loot[i].toUpperCase().replace("‘", "").replace("’", ""));
-									String sBiome = biome[i].replace("‘", "").replace("’", "");
-									if (sBiome.toLowerCase() == "all"){
-										sBiome = loc.getWorld().getBiome(loc.getBlockX(), loc.getBlockZ()).toString();
-									}
-									if (lookup != null && sBiome == loc.getWorld().getBiome(loc.getBlockX(), loc.getBlockZ()).toString()){
+									if (lookup != null){
 										ItemStack dLoot = new ItemStack(lookup);//TODO add checks
 										cInv.addItem(dLoot);
 									}else{
